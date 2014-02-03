715a43b67493c0b1df8a987ad71d8cc1bb1fde85
BraydenYates <braydengyates@aol.com>
2014-02-03 17:23:50 -0600

    Update naturalselection.cpp

--- src/naturalselection.cpp
+++ src/naturalselection.cpp
@@ -1,1 +1,20 @@
 #include "RequirementsConfig.cpp"
+
+float newsSelect = NULL;
+
+using namespace std;
+
+int main()
+{
+  cout << "What type of news do you want? \n" << "0.0 = Info on selection system." << endl;
+  
+  void selectionInfo();
+}
+
+void selectionInfo()
+{
+  cout << "Our news system is simple, The number before the decimal is a large category, IE Cars, and the number after is \n" \ 
+  << "a subcategory. Doing <NUMBER>.99 will list the subcategories in the selected category." << endl;
+  
+  int main();
+}
